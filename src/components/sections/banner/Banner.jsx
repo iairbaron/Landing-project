12861f07a39aa5237d9e17c7a75884ab91eaf3f3
francroce <francocrocee7@gmail.com>
2023-04-27 20:49:48 -0300

    modificando el banner

--- src/components/sections/banner/Banner.jsx
+++ src/components/sections/banner/Banner.jsx
@@ -3,14 +3,14 @@ import fotoPrincipal from '../../../assets/sections/banner/fotoPrincipal.png'
 
 const Banner = () => {
   return (
-    <div className='container'>
-        <div>
-            <h1>Transformá tu negocio</h1>
-            <p>Nuestros clientes reciben asesoramiento que los ayuda a tomar mejores decisiones en base a objetividad analítica, espíritu crítico y trabajo reflexivo.</p>
-            <button>SERVICIOS</button>
+    <div style={{width: '100%', height: '40vh', backgroundColor: '#2D2641',display:'flex', alignItems: 'center'}}>
+        <div style={{width:'40%'}}>
+            <h1 style={{color:'white'}}>Transformá tu negocio</h1>
+            <p style={{color:'white'}}>Nuestros clientes reciben asesoramiento que los ayuda a tomar mejores decisiones en base a objetividad analítica, espíritu crítico y trabajo reflexivo.</p>
+            <button style={{color:'white'}}>SERVICIOS</button>
         </div>
         <div>
-
+            {/* <img src={fotoPrincipal} alt="" /> */}
         </div>
     </div>
   )
