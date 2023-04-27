e29639c86fe3eef3f5cff66fc768ad1de4ddac4d
francroce <francocrocee7@gmail.com>
2023-04-27 20:40:57 -0300

    instalando nuevas dependencias

---
+++ src/components/sections/banner/Banner.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import fotoPrincipal from '../../../assets/sections/banner/fotoPrincipal.png'
+
+const Banner = () => {
+  return (
+    <div className='container'>
+        <div>
+            <h1>Transformá tu negocio</h1>
+            <p>Nuestros clientes reciben asesoramiento que los ayuda a tomar mejores decisiones en base a objetividad analítica, espíritu crítico y trabajo reflexivo.</p>
+            <button>SERVICIOS</button>
+        </div>
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Banner
