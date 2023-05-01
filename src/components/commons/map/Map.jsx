940dc3b68b02f6074933598b096a27c6671142ca
francroce <francocrocee7@gmail.com>
2023-05-01 17:14:21 -0300

    confirmando cambios

--- src/components/commons/map/Map.jsx
+++ src/components/commons/map/Map.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {GoogleMap,withScriptjs,withGoogleMap} from "react-google-maps";
+import {GoogleMap,withScriptjs,withGoogleMap} from 'react-google-maps';
 
 
 const Map = (props) => {
