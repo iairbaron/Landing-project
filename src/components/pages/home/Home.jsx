cd4577e72cf295f30f1cd3fe6df9aba70e5b8616
iairbaron <iairbaron@gmail.com>
2023-05-01 21:46:20 -0300

    Arreglado sin carrousel en la pagina

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Contact from '../../sections/contact/Contact'
 import Diferentes from '../../sections/somosDiferentes/Diferentes'
 import Banner from '../../sections/banner/Banner'
-import Clientes from '../../sections/clientes/Clientes'
 import Navbar from '../../commons/navbar/navbar'
 import Publicidad from '../../sections/publicidad/Publicidad'
 
@@ -12,7 +11,6 @@ const Home = () => {
       <Navbar/>
       <Banner/>
       <Diferentes/>
-      <Clientes/>
       <Publicidad/>
       <Contact/>
     </div>
